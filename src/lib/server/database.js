79695e28fd1c34f94b70405b07192a814296c151
Your Name <you@example.com>
2024-10-27 15:02:08 -0700

    reverted changes

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -2,8 +2,8 @@
 import postgres from 'postgres'
 
 // see env variables in .env
-//import {PGCONNECT} from '$env/static/private';
+import {PGCONNECT} from '$env/static/private';
 
-const sql = postgres(process.env.PGCONNECT, {} )
+const sql = postgres(PGCONNECT, {} )
 
 export default sql;
